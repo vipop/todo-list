f481d89487bfafb0520a676dcd49696bcb49eca5
Vlad Pop <vipop94@gmail.com>
2017-03-13 13:53:37 -0400

    complete checkNote() and removeNote()

--- notes.js
+++ notes.js
@@ -1,27 +1,81 @@
 var notes = angular.module("notes", []);
 
+notes.service("notesService", function(){
+	this.clean = function (x) {
+
+        return clean;
+    }
+});
+
 notes.controller("notesController", function($scope, $http){
 	var i;
+	$scope.all = [];
 	$scope.todos = [];
+	$scope.completes = [];
 	$http.get('todo.json')
        .then(function(res){
 		   var data = res.data;
 		   	//console.log(data);
 		   	for (i = 0; i < data.todo.length; i++) {
- 	  			$scope.todos.push(data.todo[i]);
+				if (data.todo[i].complete == "true") $scope.completes.push(data.todo[i]);
+				else $scope.todos.push(data.todo[i]);
+				$scope.all.push(data.todo[i]);
  	  		}
         });
 	$scope.addNote = function() {
-		var title = document.getElementById("title").value;
+		var task = document.getElementById("title").value;
 		var description = document.getElementById("description").value;
 
-		if (title != '' && description != '') $scope.todos.push({"task":title, "complete":"false", "description":description});
+		if (title != '' && description != '') {
+			$scope.todos.push({"task":task, "complete":"false", "description":description});
+			$scope.all.push({"task":task, "complete":"false", "description":description});
+		}
 	}
 	$scope.checkNote = function(event) {
-		event.currentTarget.parentNode.parentNode.style["background-color"] = "darkseagreen";
+		//event.currentTarget.parentNode.parentNode.style["background-color"] = "darkseagreen";
+		var unique = [];
+		var list = $scope.todos;
+		var task = event.currentTarget.parentNode.parentNode.firstChild.innerText;
+		var description = event.currentTarget.parentNode.parentNode.childNodes[2].innerText;
+		for (i = 0; i < list.length; i++) {
+			if (list[i].task != task && list[i].description != description) {
+				unique.push(list[i]);
+			}
+		}
+		$scope.todos = unique;
+		$scope.completes.push({"task":task, "complete":"true", "description":description});
+
 	}
 	$scope.removeNote = function(event) {
-		document.getElementsByTagName("body")[0].removeChild(event.currentTarget.parentNode.parentNode);
+		var i;
+		var unique = [];
+		var index = -1;
+		var notes = document.getElementsByClassName("note");
+		var note = event.currentTarget.parentNode.parentNode;
+		if (event.currentTarget.parentNode.parentNode.style["background-color"] == "darkseagreen") list = $scope.completes;
+		else list = $scope.todos;
+		/*for (i = 0; i < notes.length; i++) {
+			if (notes[i] == note) {
+				index = i;
+			}
+		}*/
+		var task = event.currentTarget.parentNode.parentNode.firstChild.innerText;
+		var description = event.currentTarget.parentNode.parentNode.childNodes[2].innerText;
+		//var task = list[index].task;
+		//var description = list[index].description;
+		for (i = 0; i < list.length; i++) {
+			if (list[i].task != task && list[i].description != description) {
+				unique.push(list[i]);
+			}
+		}
+		if (event.currentTarget.parentNode.parentNode.style["background-color"] == "darkseagreen") {
+			$scope.completes = unique;
+			$scope.all = $scope.completes.concat($scope.todos);
+		} else {
+			$scope.todos = unique;
+			$scope.all = $scope.todos.concat($scope.completes);
+		}
+		document.getElementsByTagName("body")[0].removeChild(note);
 	}
 	var upload = document.getElementById("upload");
 	document.getElementsByTagName("body")[0].removeAttribute("style");
@@ -29,7 +83,7 @@ notes.controller("notesController", function($scope, $http){
 });
 
 notes.filter('customFilter', function() {
-    return function(collection) {
+    return function(collection, completes) {
 		var output = [];
 		var i, j;
 		for (i = 0; i < collection.length; i++) {
@@ -39,9 +93,6 @@ notes.filter('customFilter', function() {
 			else {
 				for (j = 0; j < output.length; j++) {
 					var item = output[j];
-					//console.log(item);
-					//console.log(todo);
-					//console.log("item[title] = " + item["task"] + " and todo[title] = " + todo["task"]);
 					if (item["task"] == todo["task"]) {
 						console.log("same title");
 						found = "true";
@@ -52,13 +103,12 @@ notes.filter('customFilter', function() {
 					}
 			    }
 				if (found == "false") {
-					console.log("adding unique note");
 					output.push(todo);
 				}
 				else console.log("duplicate task!");
 			}
 		}
-		console.log("filtering done");
+		//console.log("filtering done");
 	    return output;
     };
 });
